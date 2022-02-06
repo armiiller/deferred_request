7806e4f1f6fe0526140c37ac6ebfb583e74bcb73
Austin Miller <austinrmiller1991@gmail.com>
2022-02-06 15:00:00 -0600

    Set the model parent class to ActiveRecord::Base

--- lib/deferred_request.rb
+++ lib/deferred_request.rb
@@ -4,5 +4,5 @@ require "deferred_request/engine"
 module DeferredRequest
   # Your code goes here...
   mattr_accessor :model_parent_class
-  @@model_parent_class = "ApplicationRecord"
+  @@model_parent_class = "ActiveRecord::Base"
 end

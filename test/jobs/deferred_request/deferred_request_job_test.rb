2c3c46fa6927ea04d0b2d6428f78b0a00afd14c2
Austin Miller <austinrmiller1991@gmail.com>
2026-06-30 13:02:33 -0700

    Add some meaningful tests

--- test/jobs/deferred_request/deferred_request_job_test.rb
+++ test/jobs/deferred_request/deferred_request_job_test.rb
@@ -2,8 +2,24 @@ require "test_helper"
 
 module DeferredRequest
   class DeferredRequestJobTest < ActiveJob::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "perform processes the deferred request and stores the result" do
+      deferred_request = DeferredRequest.create!(
+        controller: "DeferredRequest::TestController",
+        action: "status_callback",
+        params: {"answer" => 21}
+      )
+
+      DeferredRequestJob.perform_now(deferred_request.id)
+
+      deferred_request.reload
+      assert_equal "complete", deferred_request.status
+      assert_equal 21, deferred_request.result
+    end
+
+    test "perform does not raise when the deferred request no longer exists" do
+      assert_nothing_raised do
+        DeferredRequestJob.perform_now(-1)
+      end
+    end
   end
 end
